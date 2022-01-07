c9580328827fd69800544260eab6fb31edff9185
Craig <indigomx9@gmail.com>
2022-01-06 19:14:30 -0500

    Added all CRUD Functions.

--- src/controllers/actorCon.ts
+++ src/controllers/actorCon.ts
@@ -29,8 +29,48 @@ async (req, res, next) => {
     }
 };
 
+export const GetOneActor: express.RequestHandler =
+async (req, res, next) => {
+    try {
+        const actor: Actors =
+        await Actors.findOneOrFail(req.params.id);
+        return res.status(200).json(actor);
+    } catch (error) {
+        res.status(500).json(error);
+        next(error);
+    }
+};
 
+export const UpdateActor: express.RequestHandler =
+async (req, res, next) => {
+    try {
+        const actor: Actors =
+        await Actors.findOneOrFail(req.params.id);
+        actor.title = req.body.title;
+        actor.firstname = req.body.firstname;
+        actor.lastname = req.body.lastname;
+        actor.age = req.body.age;
+        actor.info = req.body.info;
+        await actor.save();
+        return res.status(200).json(actor);
+    } catch (error) {
+        res.status(500).json(error);
+        next(error);
+    }
+};
 
+export const DeleteActor: express.RequestHandler =
+async (req, res, next) => {
+    try {
+        const actor: Actors =
+        await Actors.findOneOrFail(req.params.id);
+        await actor.remove();
+        return res.status(200).json("The Actor was Deleted!");
+    } catch (error) {
+        res.status(500).json(error);
+        next(error);
+    }
+};
 
 
 
--- src/routes/actorRt.ts
+++ src/routes/actorRt.ts
@@ -1,10 +1,13 @@
 import express from "express";
-import { CreateActor, 
-    GetAllActors } from "../controllers/actorCon";
+import { CreateActor, GetAllActors, GetOneActor, UpdateActor, 
+    DeleteActor } from "../controllers/actorCon";
 
 export const actorRt: express.Router = express.Router();
     actorRt.post("/", CreateActor);
     actorRt.get("/",  GetAllActors);
+    actorRt.get("/:id", GetOneActor);
+    actorRt.put("/:id", UpdateActor);
+    actorRt.delete("/:id", DeleteActor)
 
 
 
